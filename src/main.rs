b5e54628a73a8929e1f0c9f626d42b0cf55a8054
Daan Schutte <daan.schutte@icloud.com>
2021-11-07 22:57:08 +0300

    Initial directory traversal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::{fs, io};
+use std::path::PathBuf;
+
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "Imaginfo", about = "An about section about Imaginfo")]
+struct Opt {
+    /// Input file
+    #[structopt(parse(from_os_str))]
+    input: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+    println!("{:?}", &opt);
+    let path = opt.input;
+    let entries = find_files(&path);
+
+    for file in &entries {
+        println!("{:?}", file)
+    }
+}
+
+fn find_files(path: &PathBuf) -> Result<Vec<PathBuf>, Box<dyn std::error::Error>> {
+    let mut entries = fs::read_dir(path)?
+        .map(|res| res.map(|e| e.path()))
+        .collect::<Result<Vec<_>, io::Error>>()?;
+
+    entries.sort();
+
+    Ok(entries)
 }
