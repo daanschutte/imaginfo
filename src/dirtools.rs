3c59a7ca49aeb0024d69943755bd6e140db1a07b
Daan Schutte <daan.schutte@icloud.com>
2021-11-09 20:10:26 +0300

    Simplify to one method and add max depth option.

--- src/dirtools.rs
+++ src/dirtools.rs
@@ -1,42 +1,26 @@
-use std::fs;
-use std::fs::DirEntry;
 use std::path::{Path, PathBuf};
 
 use walkdir::WalkDir;
 
-pub(crate) fn find_files_recurse(path: &Path) -> Vec<PathBuf> {
-    WalkDir::new(path)
+pub(crate) fn find_files_recurse(
+    path: &Path,
+    max_depth: usize,
+) -> Result<Vec<PathBuf>, Box<dyn std::error::Error>> {
+    let paths = WalkDir::new(path)
+        .min_depth(1)
+        .max_depth(max_depth)
         .into_iter()
-        .filter_map(|e| e.ok())
-        .filter(|e| !is_hidden_walkdir(e))
+        .map(|e| e.unwrap())
+        .filter(|e| !is_hidden(e))
         .filter(|e| !e.path_is_symlink())
         .filter(|e| !e.file_type().is_dir())
         .map(|e| e.path().to_path_buf())
-        .collect::<Vec<PathBuf>>()
+        .collect::<Vec<PathBuf>>();
+
+    Ok(paths)
 }
 
-pub(crate) fn find_files_no_recurse(path: &Path) -> Vec<PathBuf> {
-    fs::read_dir(&path)
-        .unwrap()
-        .into_iter()
-        .filter_map(|e| e.ok())
-        .filter(|e| !is_hidden(e))
-        .filter(|e| !e.file_type().unwrap().is_dir())
-        .filter(|e| !e.file_type().unwrap().is_symlink())
-        .map(|e| e.path())
-        .collect::<Vec<PathBuf>>()
-}
-
-// TODO make generic
-fn is_hidden_walkdir(entry: &walkdir::DirEntry) -> bool {
-    entry
-        .file_name()
-        .to_str()
-        .map(|s| s.starts_with('.'))
-        .unwrap_or(false)
-}
-
-fn is_hidden(entry: &DirEntry) -> bool {
+fn is_hidden(entry: &walkdir::DirEntry) -> bool {
     entry
         .file_name()
         .to_str()
--- src/main.rs
+++ src/main.rs
@@ -11,39 +11,42 @@ name = "Imaginfo",
 about = "An application to give insights into photo metadata"
 )]
 struct Opt {
-    /// Input file
+    /// Root directory from which to search
     #[structopt(parse(from_os_str))]
     path: PathBuf,
 
-    /// Enable debugging
-    #[structopt(short, long)]
+    /// Enable debug mode
+    #[structopt(short = "D", long)]
     debug: bool,
 
-    /// Follow directories recursively
+    /// Follow directories recursively up to the maximum depth
     #[structopt(short, long)]
     recurse: bool,
+
+    /// Set maximum depth to search
+    #[structopt(short = "d", long = "depth", default_value = "1")]
+    max_depth: usize,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let opt = Opt::from_args();
     let debug = opt.debug;
+    let recurse = opt.recurse;
+    let max_depth: usize = if recurse { usize::MAX } else { opt.max_depth };
+    let path = &opt.path;
 
     // TODO remove / improve
     if debug {
         println!("{:?}", &opt);
     }
 
-    let path = opt.path;
-    let recurse = opt.recurse;
+    let files = dirtools::find_files_recurse(&path, max_depth);
 
-    let files = if recurse {
-        dirtools::find_files_recurse(&path)
-    } else {
-        dirtools::find_files_no_recurse(&path)
-    };
-
-    println!("Dirs:");
-    files.into_iter().for_each(|e| println!("{}", e.display()));
+    println!("Paths:");
+    files
+        .into_iter()
+        .flatten()
+        .for_each(|e| println!("{}", e.display()));
 
     Ok(())
 }
