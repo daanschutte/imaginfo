b1e9819835b133e4346221f0ab37e9783876163d
Daan Schutte <daan.schutte@icloud.com>
2021-11-09 09:26:08 +0300

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::error::Error;
 use std::fs;
+use std::fs::FileType;
 use std::path::PathBuf;
 
 use structopt::StructOpt;
@@ -19,45 +20,55 @@ fn main() -> Result<(), Box<dyn Error>> {
     let path = opt.input;
     // let entries = find_files(&path);
 
-    find_files_recursive(&path);
-    find_files_no_recurse(&path);
+    let recurse = find_files_recursive(&path);
+    println!("Recursive dirs:");
+    recurse
+        .into_iter()
+        .for_each(|e| println!("{}", e.display()));
+
+    let no_recurse = find_files_no_recurse(&path);
+    println!("Non recursive dirs:");
+    no_recurse
+        .into_iter()
+        .for_each(|e| println!("{}", e.display()));
 
     Ok(())
 }
 
-fn find_files_recursive(path: &PathBuf) -> Result<Vec<PathBuf>, Box<dyn std::error::Error>> {
-    let entries = WalkDir::new(path)
+fn find_files_recursive(path: &PathBuf) -> Vec<PathBuf> {
+    WalkDir::new(path)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| !is_hidden_walkdir(e))
+        .filter(|e| !e.file_type().is_dir())
+        .map(|e| e.path().to_path_buf())
+        .collect::<Vec<PathBuf>>()
+}
+
+fn find_files_no_recurse(path: &PathBuf) -> Vec<PathBuf> {
+    fs::read_dir(&path)
+        .unwrap()
         .into_iter()
         .filter_map(|e| e.ok())
         .filter(|e| !is_hidden(e))
-        .filter(|e| !e.file_type().is_dir())
-        .map(|e| e.path().to_path_buf())
-        .collect::<Vec<PathBuf>>();
-
-    println!("Recursive dirs:");
-    &entries.clone().into_iter().for_each(|e| println!("{}", e.display()));
-
-
-    Ok(entries)
+        .filter(|e| !e.file_type().unwrap().is_dir())
+        .map(|e| e.path())
+        .collect::<Vec<PathBuf>>()
 }
 
-fn is_hidden(entry: &DirEntry) -> bool {
-    entry.file_name()
+// TODO make generic
+fn is_hidden_walkdir(entry: &DirEntry) -> bool {
+    entry
+        .file_name()
         .to_str()
         .map(|s| s.starts_with("."))
         .unwrap_or(false)
 }
 
-
-fn find_files_no_recurse(path: &PathBuf) -> Result<Vec<PathBuf>, Box<dyn std::error::Error>> {
-    let entries = fs::read_dir(&path)?
-        .filter_map(|e| e.ok())
-        .map(|e| e.path())
-        .collect::<Vec<PathBuf>>();
-
-
-    println!("Non-recursive dirs:");
-    &entries.clone().into_iter().for_each(|e| println!("{}", e.display()));
-
-    Ok(entries)
+fn is_hidden(entry: &std::fs::DirEntry) -> bool {
+    entry
+        .file_name()
+        .to_str()
+        .map(|s| s.starts_with("."))
+        .unwrap_or(false)
 }
