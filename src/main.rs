04b3d68994b65f7f5d30bee72a27155b3badd6b7
Daan Schutte <daan.schutte@icloud.com>
2021-11-09 08:57:20 +0300

    Basic implementation of read dirs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use std::{fs, io};
+use std::error::Error;
+use std::fs;
 use std::path::PathBuf;
 
 use structopt::StructOpt;
+use walkdir::{DirEntry, WalkDir};
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "Imaginfo", about = "An about section about Imaginfo")]
@@ -11,23 +13,51 @@ struct Opt {
     input: PathBuf,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let opt = Opt::from_args();
     println!("{:?}", &opt);
     let path = opt.input;
-    let entries = find_files(&path);
+    // let entries = find_files(&path);
 
-    for file in &entries {
-        println!("{:?}", file)
-    }
+    find_files_recursive(&path);
+    find_files_no_recurse(&path);
+
+    Ok(())
 }
 
-fn find_files(path: &PathBuf) -> Result<Vec<PathBuf>, Box<dyn std::error::Error>> {
-    let mut entries = fs::read_dir(path)?
-        .map(|res| res.map(|e| e.path()))
-        .collect::<Result<Vec<_>, io::Error>>()?;
+fn find_files_recursive(path: &PathBuf) -> Result<Vec<PathBuf>, Box<dyn std::error::Error>> {
+    let entries = WalkDir::new(path)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| !is_hidden(e))
+        .filter(|e| !e.file_type().is_dir())
+        .map(|e| e.path().to_path_buf())
+        .collect::<Vec<PathBuf>>();
 
-    entries.sort();
+    println!("Recursive dirs:");
+    &entries.clone().into_iter().for_each(|e| println!("{}", e.display()));
+
+
+    Ok(entries)
+}
+
+fn is_hidden(entry: &DirEntry) -> bool {
+    entry.file_name()
+        .to_str()
+        .map(|s| s.starts_with("."))
+        .unwrap_or(false)
+}
+
+
+fn find_files_no_recurse(path: &PathBuf) -> Result<Vec<PathBuf>, Box<dyn std::error::Error>> {
+    let entries = fs::read_dir(&path)?
+        .filter_map(|e| e.ok())
+        .map(|e| e.path())
+        .collect::<Vec<PathBuf>>();
+
+
+    println!("Non-recursive dirs:");
+    &entries.clone().into_iter().for_each(|e| println!("{}", e.display()));
 
     Ok(entries)
 }
