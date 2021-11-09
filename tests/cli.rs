1005930b67f44a6941307fc6d34197d12e861502
Daan Schutte <daan.schutte@icloud.com>
2021-11-09 12:13:18 +0300

    Refactoring

--- tests/cli.rs
+++ tests/cli.rs
@@ -15,7 +15,6 @@ fn it_gives_error_if_file_does_not_exist() -> Result<(), Box<dyn std::error::Err
     Ok(())
 }
 
-
 #[test]
 fn it_finds_all_files() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("imaginfo")?;
@@ -23,8 +22,8 @@ fn it_finds_all_files() -> Result<(), Box<dyn std::error::Error>> {
     cmd.arg("tests/test_dir");
     cmd.assert()
         .success()
-        .stdout(predicate::str::contains("tests/test_dir/base1.txt"))
-        .stdout(predicate::str::contains("tests/test_dir/base2.txt"));
+        .stdout(predicate::str::contains("tests/test_dir/file0_1.txt"))
+        .stdout(predicate::str::contains("tests/test_dir/file0_2.txt"));
 
     Ok(())
 }
@@ -36,7 +35,7 @@ fn it_does_not_follow_symlinks() -> Result<(), Box<dyn std::error::Error>> {
     cmd.arg("tests/test_dir");
     cmd.assert()
         .success()
-        .stdout(predicate::str::contains("tests/test_dir/symlink_level3.txt").not());
+        .stdout(predicate::str::contains("symlink_to_file3.txt").not());
 
     Ok(())
 }
@@ -60,14 +59,14 @@ fn it_does_not_output_hidden_files() -> Result<(), Box<dyn std::error::Error>> {
     cmd.arg("tests/test_dir");
     cmd.assert()
         .success()
-        .stdout(predicate::str::contains("tests/test_dir/.hidden").not());
+        .stdout(predicate::str::contains(".hidden").not());
 
     Ok(())
 }
 
-
 #[test]
-fn it_gives_error_if_file_does_not_exist_with_recursion() -> Result<(), Box<dyn std::error::Error>> {
+fn it_gives_error_if_file_does_not_exist_with_recursion() -> Result<(), Box<dyn std::error::Error>>
+{
     let mut cmd = Command::cargo_bin("imaginfo")?;
 
     cmd.arg("tests/test_dir/does/not/exist").arg("-r");
@@ -85,13 +84,21 @@ fn it_finds_all_files_with_recursion() -> Result<(), Box<dyn std::error::Error>>
     cmd.arg("tests/test_dir").arg("-r");
     cmd.assert()
         .success()
-        .stdout(predicate::str::contains("tests/test_dir/base1.txt"))
-        .stdout(predicate::str::contains("tests/test_dir/base2.txt"))
-        .stdout(predicate::str::contains("tests/test_dir/level1/level1.txt"))
-        .stdout(predicate::str::contains("tests/test_dir/level1/level2a/level2a_1.txt"))
-        .stdout(predicate::str::contains("tests/test_dir/level1/level2a/level2a_2.txt"))
-        .stdout(predicate::str::contains("tests/test_dir/level1/lavel2b/level2b.txt"))
-        .stdout(predicate::str::contains("tests/test_dir/level1/level2a/level3/level3.txt"));
+        .stdout(predicate::str::contains("tests/test_dir/file0_1.txt"))
+        .stdout(predicate::str::contains("tests/test_dir/file0_2.txt"))
+        .stdout(predicate::str::contains("tests/test_dir/level1/file1.txt"))
+        .stdout(predicate::str::contains(
+            "tests/test_dir/level1/level2a/file2a_1.txt",
+        ))
+        .stdout(predicate::str::contains(
+            "tests/test_dir/level1/level2a/file2a_2.txt",
+        ))
+        .stdout(predicate::str::contains(
+            "tests/test_dir/level1/level2b/file2b.txt",
+        ))
+        .stdout(predicate::str::contains(
+            "tests/test_dir/level1/level2a/level3/file3.txt",
+        ));
 
     Ok(())
 }
@@ -103,8 +110,9 @@ fn it_does_not_follow_symlinks_with_recursion() -> Result<(), Box<dyn std::error
     cmd.arg("tests/test_dir").arg("-r");
     cmd.assert()
         .success()
-        .stdout(predicate::str::contains("tests/test_dir/symlink_level3.txt").not())
-        .stdout(predicate::str::contains("tests/test_dir/level1/lavel2b/symlink_base1.txt").not());
+        .stdout(predicate::str::contains("symlink_to_level1").not())
+        .stdout(predicate::str::contains("symlink_to_file0_1.txt").not())
+        .stdout(predicate::str::contains("symlink_to_file3.txt").not());
 
     Ok(())
 }
@@ -116,10 +124,7 @@ fn it_does_not_output_dirs_with_recursion() -> Result<(), Box<dyn std::error::Er
     cmd.arg("tests/test_dir").arg("-r");
     cmd.assert()
         .success()
-        .stdout(predicate::str::contains("tests/test_dir/level1").not())
-        .stdout(predicate::str::contains("tests/test_dir/level1/level2a").not())
-        .stdout(predicate::str::contains("tests/test_dir/level1/level2b").not())
-        .stdout(predicate::str::contains("tests/test_dir/level1/level2a/level3").not());
+        .stdout(predicate::str::contains("empty_dir").not());
 
     Ok(())
 }
@@ -131,10 +136,8 @@ fn it_does_not_output_hidden_files_with_recursion() -> Result<(), Box<dyn std::e
     cmd.arg("tests/test_dir").arg("-r");
     cmd.assert()
         .success()
-        .stdout(predicate::str::contains("tests/test_dir/.hidden").not())
-        .stdout(predicate::str::contains("tests/test_dir/level1/level2b/.hidden2b").not());
+        .stdout(predicate::str::contains(".hidden").not())
+        .stdout(predicate::str::contains(".hidden2b").not());
 
     Ok(())
 }
-
-
