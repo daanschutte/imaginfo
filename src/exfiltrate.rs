02e9b40660347e3ece7aa987873e5636b165f392
Daan Schutte <daan.schutte@icloud.com>
2021-11-21 21:38:30 +0300

    Add documentation

--- src/exfiltrate.rs
+++ src/exfiltrate.rs
@@ -4,6 +4,8 @@ use std::path::Path;
 use exif::{Exif, In, Rational, Tag, Value};
 use log::{debug, error};
 
+/// Reads the specified file from disk and attempts to parse all the exif data from the image file.
+/// For greater verbosity the `debug_image_info` flag should be set.
 pub(crate) fn get_exif_data(path: &Path, debug_image_info: bool) -> Result<Exif, Box<dyn Error>> {
     debug!("Reading file {}", path.display());
     let file = std::fs::File::open(path)?;
@@ -11,6 +13,7 @@ pub(crate) fn get_exif_data(path: &Path, debug_image_info: bool) -> Result<Exif,
     let exifreader = exif::Reader::new();
     let exif = exifreader.read_from_container(&mut bufreader)?;
     if debug_image_info {
+        debug!("{} has the following fields:", path.display().to_string());
         for f in exif.fields() {
             // TODO remove filters
             if f.tag != Tag::MakerNote && f.ifd_num != In::THUMBNAIL {
@@ -22,6 +25,7 @@ pub(crate) fn get_exif_data(path: &Path, debug_image_info: bool) -> Result<Exif,
     Ok(exif)
 }
 
+/// Returns the value of an `exif::Tag` that is stored as a `Value::Rational` value `In::PRIMARY`, if present.
 pub(crate) fn get_tag_rational(tag: Tag, e: &Exif) -> Option<&Rational> {
     match e.get_field(tag, In::PRIMARY) {
         Some(field) => match field.value {
